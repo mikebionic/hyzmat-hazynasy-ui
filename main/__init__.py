2ec44a9507b84166db8e858a923b7fa27eb970b9
Muhammed Jepbarov <muhammed@millione.tm>
2023-01-21 10:19:37 +0500

    updated configs to static

--- main/__init__.py
+++ main/__init__.py
@@ -14,6 +14,9 @@ db = SQLAlchemy()
 def create_app(config_class=Config):
 	app = Flask(__name__)
 	app.config.from_object(Config)
+	app.static_folder = Config.STATIC_FOLDER_LOCATION
+	app.template_folder = Config.TEMPLATE_FOLDER_LOCATION
+
 	CORS(app)
 	db.init_app(app)
 	#login_manager.init_app(app)
